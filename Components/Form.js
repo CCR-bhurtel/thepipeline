3fa8de65ee5b025e708420860cd4e1aa6a4e5aaa
cceer <sisir.sital@gmail.com>
2022-12-08 19:33:17 +0545

    frame added

--- Components/Form.js
+++ Components/Form.js
@@ -3,13 +3,14 @@ import React from "react";
 function Form({ buttontext }) {
   return (
     <form className="flex items-end justify-center mt-4">
-      {/* <iframe
-        src="https://embeds.beehiiv.com/80bf0c42-dc7f-49cc-a7ae-451c6dc72855?slim=true&embed=true"
+      <iframe
+        src="https://embeds.beehiiv.com/80bf0c42-dc7f-49cc-a7ae-451c6dc72855?slim=true"
         data-test-id="beehiiv-embed"
         frameborder="0"
         scrolling="no"
-      ></iframe> */}
-      <input
+        style="margin: 0; border-radius: 0px !important; background-color: transparent;"
+      ></iframe>
+      {/* <input
         type="text"
         className="border-b-2 border-Blue outline-0 border-x-0 borter-t-0 placeholder:text-Blue font-light max-w-[50%] "
         placeholder="email"
@@ -22,7 +23,7 @@ function Form({ buttontext }) {
           {buttontext}
         </div>
         <div className="w-full h-[2px]  bg-Blue ml-2"></div>
-      </div>
+      </div> */}
     </form>
   );
 }
